af967ab9730aee953430246f4b19e76d8f3c03b1
Wei QIn <xiuman893821@gmail.com>
2018-11-12 16:35:51 -0500

    update
    make the code have the dynamic potential, replace magic word with variable to make it automatic

---
+++ curWindow.js
@@ -0,0 +1,39 @@
+var localnewref;
+function searching(dom) {
+    //var localdom = dom;
+    if (localnewref) {
+        return localnewref;
+    }
+    var count;
+    if (!dom) {
+        return;
+    }
+    count = dom.childElementCount;
+    var allchil = dom.children;
+    for (var i = 0; i < count; i++) {
+        var chil = allchil[i];
+        if (chil.tagName == "IFRAME" && chil.src != "" && chil.id != 'buffer') {
+            localnewref = chil.src
+            return localnewref;
+        }
+        else {
+            searching(chil);
+        }
+    }
+}
+//var dom = tabs[0].window.document.body;
+//var localnewref;
+//searching(dom);
+//var myWindos = window.open(localnewref, "myWindow", "resizable");
+
+chrome.runtime.onMessage.addListener(function (msg,sender) {
+    if (msg.embed == "on") {
+                                     console.log("window recive");
+        var dom = window.document.body;
+                                     
+        searching(dom);
+        window.open(localnewref, "myWindow", "resizable");
+        
+    }
+else{console.log("no recive");}
+});
